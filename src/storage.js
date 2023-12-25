8860d57f601550900a6ffa6a7600f38e4e94a565
MadOne <madone85@googlemail.com>
2023-12-25 20:29:08 +0100

    add storage

---
+++ src/storage.js
@@ -0,0 +1,43 @@
+import ToDoList from "./ToDoList";
+import ToDoItem from "./toDoItem";
+import ToDoLists from "./toDoLists";
+
+export default class Storage {
+	constructor(myToDoListsObject) {
+		this.myToDoListsObject = myToDoListsObject;
+	}
+	serialize() {
+		let mybackup = JSON.stringify(this.myToDoListsObject, (key, value) => {
+			if (value && typeof value === "object") {
+				value.__type = value.constructor.name;
+			}
+
+			return value;
+		});
+		localStorage.setItem("backup", mybackup);
+		//console.log("values stored");
+		//console.log(mybackup);
+	}
+
+	deserialize() {
+		const classes = {
+			Object,
+			ToDoLists,
+			ToDoList,
+			ToDoItem,
+		};
+		const myJsonString = localStorage.getItem("backup");
+		//console.log(myJsonString);
+
+		this.myToDoListsObject = JSON.parse(myJsonString, (key, value) => {
+			if (value && typeof value === "object" && value.__type) {
+				const DynamicClass = classes[value.__type];
+				value = Object.assign(new DynamicClass(), value);
+				delete value.__type;
+			}
+			return value;
+		});
+		console.log(this.myToDoListsObject);
+		return this.myToDoListsObject;
+	}
+}
